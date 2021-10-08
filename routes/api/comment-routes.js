b0994520d0a23750afa06b22e1cfa6226adfbaff
Deku <zachdsamson@gmail.com>
2021-10-07 19:11:28 -0700

    small change

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -4,4 +4,10 @@ const {
     removeComment
 } = require('../../controllers/comment-controller');
 
+// /api/comment/<pizzaId>
+router.route('/:pizzaId').post(addComment);
+
+// /api/comments/<pizzaId>/<commentId>
+router.route('/:pizzaId/:commentId').delete(removeComment);
+
 module.exports = router;
