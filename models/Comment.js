ac66fad906720d7aaee8a765a2ca029be83cc506
Deku <zachdsamson@gmail.com>
2021-10-09 12:03:28 -0700

    completed reply association with comment

--- models/Comment.js
+++ models/Comment.js
@@ -1,17 +1,60 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const CommentSchema = new Schema({
-    writtenBy: {
-        type: String
-    },
-    commentBody: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
+const ReplySchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent comment _id
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        replyBody: {
+            type: String
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    }, 
+    {
+        toJSON: {
+            getters: true
+        }
     }
-});
+);
+
+const CommentSchema = new Schema(
+    {
+        writtenBy: {
+            type: String
+        },
+        commentBody: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        replies: [ReplySchema]
+    },
+    {
+        toJSON: {
+            virturals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// get total count of comments and replies on retrieval
+CommentSchema.virtual('replyCount').get(function() {
+    return this.replies.length;
+  });
 
 const Comment = model('Comment', CommentSchema);
 
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,13 +1,23 @@
 const router = require('express').Router();
 const {
     addComment,
-    removeComment
+    removeComment,
+    addReply,
+    removeReply
 } = require('../../controllers/comment-controller');
 
 // /api/comment/<pizzaId>
 router.route('/:pizzaId').post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router
+    .route('/:pizzaId/:commentId')
+    .put(addReply)
+    .delete(removeComment);
+
+// /api/comments/<pizzId>/<commentId>/<replyId>
+router
+    .route('/:pizzaId/:commentId/:replyId')
+    .delete(removeReply);
 
 module.exports = router;
